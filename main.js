6b4f4019c66c1f49cd92b9866a3368016cd5cdba
aunabbas2 <159635620+aunabbas2@users.noreply.github.com>
2024-02-18 23:20:51 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+let person_name = "aun";
+let messege = ` hello ${person_name},hope u r doing will these days`;
+console.log(messege);
---
+++ main.ts
@@ -0,0 +1,3 @@
+let person_name : string = "aun";
+let messege : string = ` hello ${person_name},hope u r doing will these days`;
+console.log(messege);
