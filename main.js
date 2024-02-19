2b60f394434ad57f720fbe08acbc5cb87380c513
aunabbas2 <159635620+aunabbas2@users.noreply.github.com>
2024-02-18 23:34:04 -0800

    3.namecases

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
 "use strict";
-// Store the locations in a array. Make sure the array is not in alphabetical order.//
-let places = ['Italy', 'UAE', 'Paris', 'USA', 'England'];
-//Print your array in its original order.
-console.log(places); //originol
-//Print your array in alphabetical order without modifying the actual list.
-console.log([...places].sort()); //copy
-//Show that your array is still in its original order by printing it.
-console.log(places);
-//Print your array in reverse alphabetical order without changing the order of the original list.
-console.log([...places].sort().reverse());
-//Show that your array is still in its original order by printing it again.
-console.log(places);
-//Reverse the order of your list. Print the array to show that its order has changed.
-console.log(places.reverse());
-//Reverse the order of your list again. Print the list to show it’s back to its original order.
-console.log(places.reverse());
-// Sort your array so it’s stored in alphabetical order. Print the array to show that its order has changed
-console.log(places.sort().reverse());
+let personName = "aun abbas";
+let lowercaseName = personName.toLowerCase();
+console.log(lowercaseName);
+let uppercaseName = personName.toUpperCase();
+console.log(uppercaseName);
--- main.ts
+++ main.ts
@@ -1,26 +1,7 @@
-// Store the locations in a array. Make sure the array is not in alphabetical order.//
-let places : string[] = ['Italy','UAE','Paris','USA','England'];
+let personName : string ="aun abbas";
 
-//Print your array in its original order.
-console.log(places);                                        //originol
+let lowercaseName : string = personName.toLowerCase();   //lower case//
+console.log(lowercaseName);
 
-//Print your array in alphabetical order without modifying the actual list.
-console.log([...places].sort());//copy
-
-//Show that your array is still in its original order by printing it.
-console.log(places);
-
-//Print your array in reverse alphabetical order without changing the order of the original list.
-console.log([...places].sort().reverse());
-
-//Show that your array is still in its original order by printing it again.
-console.log(places);
-
-//Reverse the order of your list. Print the array to show that its order has changed.
-console.log(places.reverse());
-
-//Reverse the order of your list again. Print the list to show it’s back to its original order.
-console.log(places.reverse());
-
-// Sort your array so it’s stored in alphabetical order. Print the array to show that its order has changed
-console.log(places.sort().reverse());
+let uppercaseName : string = personName.toUpperCase();   //upper case//
+console.log(uppercaseName);
