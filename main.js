9134a62b6b30a5b391f6ef4e266b865516de6f4f
aunabbas2 <159635620+aunabbas2@users.noreply.github.com>
2024-02-18 23:23:12 -0800

    5.famous quote

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 "use strict";
-console.log("Albert Einstein once said, “A person who never made a mistake never tried anything new.”");
+let famousPerson = "Albert Einstien";
+let messege = '"A person who never made a mistake never tried anything new"';
+console.log(`${famousPerson} once said, ${messege}`);
