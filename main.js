0050c21bcb25e35f0ec94bc883cf49268e75e4ac
aunabbas2 <159635620+aunabbas2@users.noreply.github.com>
2024-02-18 23:30:28 -0800

    15.changing guestlist

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 "use strict";
 let list = ['imran khan', 'nawaz sharif', 'zardari'];
 for (let i = 0; i < list.length; i++) {
-    console.log('dear Mr' + list[i] + ',\n\nit is our pleasure to invite you in party.\n\nTHANKYOU!');
+    console.log('dear Mr' + list[i] + ',\n\nit is our pleasure to invite you in party.\n\nTHANKYOU!\n\n');
 }
+let absent_guest = 'imran khan';
+let new_guest = 'SIR KAMRAN KHAN TESSORI';
+list[0] = new_guest;
+for (let i = 0; i < list.length; i++) { //again loop//
+    console.log('dear Mr' + list[i] + ',\n\nit is our pleasure to invite you in party.\n\nTHANKYOU!\n\n');
+}
+console.log(`MR ${absent_guest} is not coming `);
--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
 let list: string[] = ['imran khan','nawaz sharif','zardari'];
 for(let i=0; i<list.length;i++){
 
-    console.log('dear Mr'+list[i] + ',\n\nit is our pleasure to invite you in party.\n\nTHANKYOU!')
+    console.log('dear Mr'+list[i] + ',\n\nit is our pleasure to invite you in party.\n\nTHANKYOU!\n\n')
 }
+let absent_guest : string = 'imran khan';
+let new_guest : string = 'SIR KAMRAN KHAN TESSORI';
+
+list[0] = new_guest;
+
+for(let i=0; i<list.length;i++){                     //again loop//
+
+    console.log('dear Mr'+list[i] + ',\n\nit is our pleasure to invite you in party.\n\nTHANKYOU!\n\n')
+}
+
+console.log(`MR ${absent_guest} is not coming `)
+
