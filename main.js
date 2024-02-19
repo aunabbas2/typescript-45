d8cbb2e4f052fb9e88baa151c6282b1bb89b0e00
aunabbas2 <159635620+aunabbas2@users.noreply.github.com>
2024-02-18 23:31:55 -0800

    18.shrinking guestlist

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ let list = ['imran khan', 'nawaz sharif', 'zardari'];
 let absent_guest = 'imran khan';
 let new_guest = 'SIR KAMRAN KHAN TESSORI';
 list[0] = new_guest;
-for (let i = 0; i < list.length; i++) { //again loop//
-    console.log('dear Mr' + list[i] + ',\n\nit is our pleasure to invite you in party.\n\nTHANKYOU!\n\n');
-}
+//for(let i=0; i<list.length;i++){                     //again loop//
+//  console.log('dear Mr'+list[i] + ',\n\nit is our pleasure to invite you in party.\n\nTHANKYOU!\n\n')
+//}
 console.log(`MR ${absent_guest} is not coming `);
 console.log('good news! we find big table so we are invited more 3 guest');
 list.unshift('sir zia khan');
@@ -17,3 +17,13 @@ list.push('bilawal bubloo');
 for (let i = 0; i < list.length; i++) { //again loop//
     console.log('dear Mr' + list[i] + ',\n\nit is our pleasure to invite you in party.\n\nTHANKYOU!\n\n');
 }
+console.log('\nsorry we cant arrange big table,only two pwople will be invited');
+while (list.length > 2) {
+    let remove_guest = list.pop();
+    console.log(`sorry mr. ${remove_guest}, you r notinvited for dinner`);
+}
+for (let i = 0; i < list.length; i++) { //again loop//
+    console.log('dear Mr' + list[i] + ',\n\nyou are still invited .\n\nTHANKYOU!\n\n');
+}
+list.splice(0, 2);
+console.log(list);
--- main.ts
+++ main.ts
@@ -8,10 +8,10 @@ let new_guest : string = 'SIR KAMRAN KHAN TESSORI';
 
 list[0] = new_guest;
 
-for(let i=0; i<list.length;i++){                     //again loop//
+//for(let i=0; i<list.length;i++){                     //again loop//
 
-    console.log('dear Mr'+list[i] + ',\n\nit is our pleasure to invite you in party.\n\nTHANKYOU!\n\n')
-}
+  //  console.log('dear Mr'+list[i] + ',\n\nit is our pleasure to invite you in party.\n\nTHANKYOU!\n\n')
+//}
 
 console.log(`MR ${absent_guest} is not coming `)
 console.log('good news! we find big table so we are invited more 3 guest')
@@ -23,3 +23,18 @@ for(let i=0; i<list.length;i++){                     //again loop//
 
     console.log('dear Mr'+list[i] + ',\n\nit is our pleasure to invite you in party.\n\nTHANKYOU!\n\n')
 }
+console.log('\nsorry we cant arrange big table,only two pwople will be invited');
+
+while(list.length > 2){
+   let remove_guest = list.pop();
+
+
+console.log(`sorry mr. ${remove_guest}, you r notinvited for dinner`);
+}
+
+for(let i=0; i<list.length;i++){                     //again loop//
+
+    console.log('dear Mr'+list[i] + ',\n\nyou are still invited .\n\nTHANKYOU!\n\n')
+}
+list.splice(0,2);
+console.log(list);
