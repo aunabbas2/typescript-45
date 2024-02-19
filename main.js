056590aa6677235b741e80875a7b17dd25ab48c2
aunabbas2 <159635620+aunabbas2@users.noreply.github.com>
2024-02-18 23:28:23 -0800

    13.own array

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 "use strict";
-let names = ['aun', 'ayesha', 'bilal', 'daniyal', 'aashir'];
-const messege = 'welcome to typescript class';
-for (let i = 0; i < names.length; i++) {
-    console.log(messege + names[i]);
+let transportation = ['motorcycle', 'car', 'cycle', 'train'];
+for (let i = 0; i < transportation.length; i++) {
+    console.log('i would like to own a' + transportation[i]);
 }
--- main.ts
+++ main.ts
@@ -1,8 +1,4 @@
-let names : string[] = ['aun','ayesha','bilal','daniyal','aashir'];
-
-
-const messege :string = 'welcome to typescript class'
-
-for(let i=0; i<names.length;i++){
-    console.log(messege + names[i]);
+let transportation: string[] =['motorcycle','car','cycle','train'];
+for(let i=0;i<transportation.length;i++){
+    console.log('i would like to own a'+ transportation[i])
 }
