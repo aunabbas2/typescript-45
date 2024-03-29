cad0d0d815c59115c0f9beb70fa065d58ad4cb1d
aunabbas2 <159635620+aunabbas2@users.noreply.github.com>
2024-03-29 10:46:48 -0700

    22.national error

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 "use strict";
-//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
-let person = { name: 'AUN ABBAS', Fname: 'anwar', age: 22 };
-console.log(person);
+// Q 22 intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. 
+//Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+let transportation = ['motorcycle', 'car', 'cycle', 'train'];
+console.log(transportation[3]);
--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+// Q 22 intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. 
+//Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
 
-let person:{name:string, Fname:string , age:number} = {name:'AUN ABBAS',Fname:'anwar',age:22};
-console.log(person);
+let transportation: string[] =['motorcycle','car','cycle','train'];
+
+console.log(transportation[7]);
+
+
+
