37da356ecd0455c9697945f3b7366f4f15e8ebe8
aunabbas2 <159635620+aunabbas2@users.noreply.github.com>
2024-05-04 00:23:58 -0700

    31.no user

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 "use strict";
-// Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. 
-// Loop through the array, and print a greeting to each user:
-const userName = ["admin", "aun", "bilal", "ashir", "dani"];
-for (let i = 0; i < userName.length; i++) {
-    // If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report? Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
-    if (userName[i] === "admin") {
-        console.log("hello admin, would you like to see a status report?");
-    }
-    else {
-        console.log(`Hello ${userName[i]}, thank you for logging in again.`);
-    }
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+let userName = ['admin', 'aun', 'dani', 'bilal', 'ashir'];
+if (userName.length === 0) {
+    console.log("We need to find some users!");
+}
+// Remove all of the usernames from your array, and make sure the correct message is printed.
+else {
+    userName = []; //empty array//
+    console.log("all usser names removed" + userName.length);
 }
--- main.ts
+++ main.ts
@@ -1,21 +1,14 @@
-// Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. 
-// Loop through the array, and print a greeting to each user:
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
 
+let userName: string[] = ['admin','aun','dani','bilal','ashir'];
 
+if(userName.length === 0){
 
-
-
-
-
-const userName : string[]=["admin","aun","bilal","ashir","dani"];
-for(let i =0; i<userName.length;i++){
-
-    // If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report? Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
-if(userName[i]==="admin"){
-    console.log("hello admin, would you like to see a status report?")
+    console.log("We need to find some users!");
 }
-
+// Remove all of the usernames from your array, and make sure the correct message is printed.
 else{
-    console.log(`Hello ${userName[i]}, thank you for logging in again.`)
-}
+userName =[];                    //empty array//
+console.log("all usser names removed" + userName.length);
 }
