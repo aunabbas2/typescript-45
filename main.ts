045ee31506fb42ca29ab54111742dbf160295be6
aunabbas2 <159635620+aunabbas2@users.noreply.github.com>
2024-05-04 00:26:00 -0700

    32.check username

--- main.ts
+++ main.ts
@@ -1,14 +1,21 @@
-// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
-// • If the list is empty, print the message We need to find some users!
+// Make a list of five or more usernames called current_users.
+let current_users : string[]=["Aun","TaHa","Tabish","Bilal","KausAr"];
 
-let userName: string[] = ['admin','aun','dani','bilal','ashir'];
 
-if(userName.length === 0){
+// Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+let new_users = ["kkk","kamal","sadia","TaHa","KausAr"];
 
-    console.log("We need to find some users!");
+// Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+
+// • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
+new_users.forEach(newUsername =>){
+
+
+    let lowerCase = newUsername .tolowercase();
+    if(current_users.map(c_user => c_user.toLowerCase())).includes(lowerCase){
+console.log(`the username ${newusername} is not available.pls write diffrent username`);
+    }
 }
-// Remove all of the usernames from your array, and make sure the correct message is printed.
 else{
-userName =[];                    //empty array//
-console.log("all usser names removed" + userName.length);
+    console.log(`the username `${newusername} is available`)
 }
