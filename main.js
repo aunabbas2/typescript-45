302918905a44cfe1320d762359b0adfc59f4aaa7
aunabbas2 <159635620+aunabbas2@users.noreply.github.com>
2024-05-17 21:06:38 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 "use strict";
-// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
-// • If the list is empty, print the message We need to find some users!
-let userName = ['admin', 'aun', 'dani', 'bilal', 'ashir'];
-if (userName.length === 0) {
-    console.log("We need to find some users!");
-}
-// Remove all of the usernames from your array, and make sure the correct message is printed.
-else {
-    userName = []; //empty array//
-    console.log("all usser names removed" + userName.length);
+// Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+// • Store the numbers 1 through 9 in a array.
+// • Loop through the array.
+// • Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let num of number) { //array ka name likhte jo k number hai yah py
+    let ordinalEnding; //variable bnaya
+    if (num === 1) {
+        ordinalEnding = "first";
+    }
+    else if (num === 2) {
+        ordinalEnding = "seondnd";
+    }
+    else if (num === 3) {
+        ordinalEnding = "third";
+    }
+    else if (num === 2) {
+        ordinalEnding = "fourth";
+    }
+    else if (num === 2) {
+        ordinalEnding = "five";
+    }
+    else if (num === 2) {
+        ordinalEnding = "six";
+    }
+    else if (num === 2) {
+        ordinalEnding = "seven";
+    }
+    else if (num === 2) {
+        ordinalEnding = "eight";
+    }
+    else {
+        ordinalEnding = "nine";
+    }
+    console.log(`${num}${ordinalEnding}`);
 }
--- main.ts
+++ main.ts
@@ -1,21 +1,42 @@
-// Make a list of five or more usernames called current_users.
-let current_users : string[]=["Aun","TaHa","Tabish","Bilal","KausAr"];
+// Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+// • Store the numbers 1 through 9 in a array.
 
+// • Loop through the array.
 
-// Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
-let new_users = ["kkk","kamal","sadia","TaHa","KausAr"];
+// • Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.
 
-// Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+let number : number[]=[1,2,3,4,5,6,7,8,9];
 
-// • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
-new_users.forEach(newUsername =>){
+for(let num of number){                              //array ka name likhte jo k number hai yah py
+let ordinalEnding : string;//variable bnaya
 
-
-    let lowerCase = newUsername .tolowercase();
-    if(current_users.map(c_user => c_user.toLowerCase())).includes(lowerCase){
-console.log(`the username ${newusername} is not available.pls write diffrent username`);
-    }
+if(num===1){
+    ordinalEnding ="first";
+}
+else if(num===2){
+    ordinalEnding="seondnd";
+}
+else if(num===3){
+    ordinalEnding="third";
+}
+else if(num===2){
+    ordinalEnding="fourth";
+}
+else if(num===2){
+    ordinalEnding="five";
+}
+else if(num===2){
+    ordinalEnding="six";
+}
+else if(num===2){
+    ordinalEnding="seven";
+}
+else if(num===2){
+    ordinalEnding="eight";
 }
 else{
-    console.log(`the username `${newusername} is available`)
+    ordinalEnding="nine";
+}
+console.log(`${num}${ordinalEnding}`);
+
 }
